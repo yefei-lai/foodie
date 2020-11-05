bb883113a9c01ceaa794e6dc2fc86b0a27b73769
laiyefei <laiyefei0710@163.com>
2020-11-05 18:07:52 +0800

    3-13 实现用户信息在页面显示

--- foodie-api/src/main/java/com/imooc/controller/PassportController.java
+++ foodie-api/src/main/java/com/imooc/controller/PassportController.java
@@ -1,12 +1,22 @@
 package com.imooc.controller;
 
+import com.imooc.pojo.Users;
 import com.imooc.pojo.bo.UserBO;
 import com.imooc.service.UserService;
+import common.imooc.utils.CookieUtils;
 import common.imooc.utils.IMOOCJSONResult;
+import common.imooc.utils.JsonUtils;
+import common.imooc.utils.MD5Utils;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Api(value = "注册登录", tags = "用于注册登录的相关接口")
 @RestController
 @RequestMapping("passport")
 public class PassportController {
@@ -14,6 +24,7 @@ public class PassportController {
     @Autowired
     private UserService userService;
 
+    @ApiOperation(value = "用户名是否存在", notes = "用户名是否存在", httpMethod = "GET")
     @GetMapping("/usernameIsExist")
     public IMOOCJSONResult usernameIsExist(@RequestParam String username){
 
@@ -32,8 +43,11 @@ public class PassportController {
         return IMOOCJSONResult.ok();
     }
 
-    @PostMapping("/register")
-    public IMOOCJSONResult register(@RequestBody UserBO userBO){
+    @ApiOperation(value = "用户注册", notes = "用户注册", httpMethod = "POST")
+    @PostMapping("/regist")
+    public IMOOCJSONResult register(@RequestBody UserBO userBO,
+                                    HttpServletRequest httpServletRequest,
+                                    HttpServletResponse httpServletResponse){
 
         String username = userBO.getUsername();
         String password = userBO.getPassword();
@@ -57,7 +71,49 @@ public class PassportController {
             return IMOOCJSONResult.errorMsg("两次密码输入不一致");
         }
         //4. 实现注册
-        userService.createUser(userBO);
+        Users users = userService.createUser(userBO);
+        //放进cookie
+        CookieUtils.setCookie(httpServletRequest, httpServletResponse, "user",
+                JsonUtils.objectToJson(users), true); //isEncode 是否加密
+
         return IMOOCJSONResult.ok();
+    }
+
+    @ApiOperation(value = "用户登录", notes = "用户登录", httpMethod = "POST")
+    @PostMapping("/login")
+    public IMOOCJSONResult login(@RequestBody UserBO userBO,
+                                 HttpServletRequest httpServletRequest,
+                                 HttpServletResponse httpServletResponse) throws Exception{
+
+        String username = userBO.getUsername();
+        String password = userBO.getPassword();
+
+        //0. 判断用户名与密码必须不能为空
+        if (StringUtils.isBlank(username) || StringUtils.isBlank(password)){
+            return IMOOCJSONResult.errorMsg("用户名或密码不能为空");
+        }
+        //实现登录
+        Users users = userService.queryUserForLogin(username, MD5Utils.getMD5Str(password));
+
+        if (null == users){
+            return IMOOCJSONResult.errorMsg("用户名或密码不正确");
+        }
+
+        users = setNullProperty(users);
+
+        CookieUtils.setCookie(httpServletRequest, httpServletResponse, "user",
+                JsonUtils.objectToJson(users), true); //isEncode 是否加密
+
+        return IMOOCJSONResult.ok(users);
+    }
+
+    private Users setNullProperty(Users users){
+        users.setPassword(null);
+        users.setMobile(null);
+        users.setEmail(null);
+        users.setCreatedTime(null);
+        users.setUpdatedTime(null);
+        users.setBirthday(null);
+        return users;
     }
 }
--- foodie-pojo/src/main/java/com/imooc/pojo/bo/UserBO.java
+++ foodie-pojo/src/main/java/com/imooc/pojo/bo/UserBO.java
@@ -1,14 +1,21 @@
 package com.imooc.pojo.bo;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
 /**
  *
  */
+@ApiModel("用户对象BO")
 public class UserBO {
 
+    @ApiModelProperty(value = "用户名", required = true)
     private String username;
 
+    @ApiModelProperty(value = "密码", required = true)
     private String password;
 
+    @ApiModelProperty(value = "确认密码", required = false)
     private String confirmPassword;
 
     public String getUsername() {
