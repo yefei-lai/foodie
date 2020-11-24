980cd2513e54128d3db0e4c7152f1794f7a926cf
laiyefei <laiyf@ebaiyihui.com>
2020-11-24 18:37:53 +0800

    学习进度--第四周1-2用户中心-查询用户信息

---
+++ foodie-service/src/main/java/com/imooc/service/center/CenterUserService.java
@@ -0,0 +1,14 @@
+package com.imooc.service.center;
+
+import com.imooc.pojo.Users;
+import com.imooc.pojo.bo.UserBO;
+
+public interface CenterUserService {
+
+    /**
+     * 根据userId查询用户信息
+     * @param userId
+     * @return
+     */
+    public Users queryUserInfo(String userId);
+}
