b0e5438da45c65638f387c43adb6a58c493c2c49
laiyefei <laiyf@ebaiyihui.com>
2021-01-26 18:12:36 +0800

    学习进度--第九周完结

---
+++ foodie-sso/src/main/java/com/imooc/Application.java
@@ -0,0 +1,21 @@
+package com.imooc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+import tk.mybatis.spring.annotation.MapperScan;
+
+@SpringBootApplication
+// 扫描 mybastis 通用mappers 所在的包
+@MapperScan(basePackages = "com.imooc.mapper")
+//扫描所有包以及相关主键包
+@ComponentScan(basePackages = {"com.imooc", "common.imooc.org.n3r.idworker"})
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
